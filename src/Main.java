be67a3800964447f6469dc5a1d90c21e66152c4a
mukabyl <mukabyl@yamdex.ru>
2023-02-14 20:43:00 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     User user = new User(8,"Tuno", 25);
+     User user1 = new User(5,"Mika", 22);
+     User user2 = new User(2,"Niko", 20);
+     UserDao userDao = new UserDao();
+     userDao.save(user);
+     userDao.save(user1);
+     userDao.save(user2);
+        System.out.println(userDao.searchById(2));
+
+
+
     }
 }
--- src/UserDao.java
+++ src/UserDao.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class UserDao {
+import java.util.ArrayList;
+
+public class UserDao {
+    private ArrayList<User> users = new ArrayList<>();
+
+    public void save(User user){
+       users.add(user);
+    }
+    public User searchById(int id){
+        for(User s: users){
+            if (s.getId()==id){
+                return s;
+            }
+        }
+        return null;
+    }
 }
